0394ac6efa8ffaa907dc20855bca7aa7b73dc90b
ComputerLover108 <ComputerLover108@gmail.com>
2016-02-01 07:10:38 +0800

    mobaxterm87

--- mytools.bat
+++ mytools.bat
@@ -1,1 +1,3 @@
 set PATH=%~dp0x64;%~dp0x64\vim;%~dp0x86;%path%;
+e:
+cd e:\public\test
--- web.bat
+++ web.bat
@@ -1,5 +1,5 @@
 rem chcp 936 'chinese'
-set PGHOME=d:\backup\portable\x64\database\pgsql
+set PGHOME=d:\public\portable\x64\database\pgsql
 set PGDATA=e:\public\database\MasterPG\data
 set PGBACKUP=e:\public\database
 set path=%PGHOME%\bin;%path%;
