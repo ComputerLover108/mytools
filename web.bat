d7f53027518f269d890e29a6445acfd6e56e0f50
ComputerLover108 <ComputerLover108@gmail.com>
2016-05-15 21:09:42 +0800

    7z 16.0

--- web.bat
+++ web.bat
@@ -1,10 +1,10 @@
 rem chcp 936 'chinese'
-set PGHOME=d:\public\portable\x64\database\pgsql
-set PGDATA=e:\public\database\MasterPG\data
-set PGBACKUP=e:\public\database
+set PGHOME=e:\public\portable\x64\database\pgsql
+set PGDATA=f:\public\database\MasterPG\data
+set PGBACKUP=f:\public\database
 set path=%PGHOME%\bin;%path%;
 
 pg_ctl -D  %PGDATA% stop
 pg_ctl -D  %PGDATA% start
 
-set PATH=e:\public\program\python34;e:\public\program\python34\Scripts;%path%;
+
